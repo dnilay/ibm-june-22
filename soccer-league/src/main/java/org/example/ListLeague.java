19876000121f1e02f38c7c7c2fdda33bad459b35
Nilay <54245048+dnilay@users.noreply.github.com>
2022-06-11 16:41:30 +0530

    Add files via upload

---
+++ soccer-league/src/main/java/org/example/ListLeague.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ListLeague
+ */
+public class ListLeague extends HttpServlet {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.println("<html><head><title>list_leagues_page</title><head><body>");
+		out.println("The Summer of Soccer Love 2004<br/>Fall Soccer League (2003)<br/>Fall Soccer League (2004)<br/>Soccer League (Spring '03)<br/>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
