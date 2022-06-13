9a9d8642b2c165eb23303f8f72c9bbc244021401
Nilay <54245048+dnilay@users.noreply.github.com>
2022-06-13 14:44:30 +0530

    Add files via upload

---
+++ soccer-league/src/main/java/org/example/controller/AddLeague.java
@@ -0,0 +1,35 @@
+package org.example.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddLeague
+ */
+public class AddLeague extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		String year=request.getParameter("year");
+		String season=request.getParameter("season");
+		String title=request.getParameter("title");
+		
+		out.println(year+"<br/>"+season+"<b/r>"+title);
+	}
+
+}
