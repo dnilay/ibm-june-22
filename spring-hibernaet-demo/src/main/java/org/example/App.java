acd24c6dc2b2244b1f828a4889bcfb0d1560ddcd
Nilay <54245048+dnilay@users.noreply.github.com>
2022-06-20 12:50:52 +0530

    Add files via upload

--- spring-hibernaet-demo/src/main/java/org/example/App.java
+++ spring-hibernaet-demo/src/main/java/org/example/App.java
@@ -1,9 +1,11 @@
 package org.example;
 
+import org.example.config.SpringConfig;
 import org.example.model.Employee;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.util.logging.XMLFormatter;
@@ -17,10 +19,11 @@ public class App
     public static void main( String[] args )
     {
         try {
-            ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+           // ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
+            ApplicationContext context=new AnnotationConfigApplicationContext(SpringConfig.class);
             SessionFactory sessionFactory=context.getBean("sessionFactory",SessionFactory.class);
             Session session= sessionFactory.openSession();
-            Employee employee=new Employee("John","Doe","john@email.com");
+            Employee employee=new Employee("Marry","Public","marry@email.com");
             session.getTransaction().begin();
             session.persist(employee);
             session.getTransaction().commit();
---
+++ spring-hibernaet-demo/src/main/java/org/example/config/SpringConfig.java
@@ -0,0 +1,49 @@
+package org.example.config;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+
+@Configuration
+@ComponentScan(basePackages = {"org.example"})
+public class SpringConfig {
+
+
+    @Bean
+    public DataSource dataSource() throws Exception {
+        ComboPooledDataSource dataSource=new ComboPooledDataSource();
+        dataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+        dataSource.setJdbcUrl("jdbc:mysql://localhost:3306/mydb");
+        dataSource.setUser("root");
+        dataSource.setPassword("root");
+        dataSource.setMinPoolSize(5);
+        dataSource.setMinPoolSize(10);
+        dataSource.setMaxIdleTime(30000);
+        return dataSource;
+    }
+    @Bean
+    public LocalSessionFactoryBean sessionFactory() throws Exception {
+        LocalSessionFactoryBean sessionFactory=  new LocalSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource());
+        sessionFactory.setPackagesToScan("org.example.model");
+        Properties properties=new Properties();
+        properties.put("hibernate.dialect","org.hibernate.dialect.MySQL5Dialect");
+        properties.put("hibernate.show_sql","true");
+        sessionFactory.setHibernateProperties(properties);
+        return sessionFactory;
+    }
+    @Bean
+    public HibernateTransactionManager transactionManager() throws Exception {
+        HibernateTransactionManager transactionManager=new HibernateTransactionManager(sessionFactory().getObject());
+        return transactionManager;
+
+    }
+}
