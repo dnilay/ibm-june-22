c28e026464adc072c76e0c95343be081394b41f7
Nilay <54245048+dnilay@users.noreply.github.com>
2022-06-20 18:15:38 +0530

    Add files via upload

--- spring-mvc-demo/src/main/java/org/example/controller/HelloController.java
+++ spring-mvc-demo/src/main/java/org/example/controller/HelloController.java
@@ -1,14 +1,32 @@
 package org.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
+	@RequestMapping
+	public String home() {
+		return "home";
+	}
+
 	@RequestMapping("/hello")
-	public String sayHello()
-	{
+	public String sayHello() {
 		return "hello";
+	}
+
+	@RequestMapping("/showForm")
+	public String showForm() {
+		return "showForm";
+	}
+
+	@RequestMapping("/processForm")
+	public String processForm(@RequestParam("t1") String str, Model theModel) {
+
+		theModel.addAttribute("NAME", str);
+		return "success";
 	}
 
 }
