53363fc62ddc2c266862aabbb03d757765b8d170
Nilay <54245048+dnilay@users.noreply.github.com>
2022-06-24 11:49:48 +0530

    Add files via upload

--- hello-world-rest-api/src/main/java/com/example/demo/controller/CustomerRestController.java
+++ hello-world-rest-api/src/main/java/com/example/demo/controller/CustomerRestController.java
@@ -5,6 +5,8 @@ import java.util.Random;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,12 @@ public class CustomerRestController {
 		Customer customer=new Customer(new Random().nextInt(1000), "John", "Doe", "john@email.com");
 		return ResponseEntity.status(HttpStatus.OK).body(customer);
 	}
+	
+	@PostMapping
+	public ResponseEntity<Customer> createNewCustomer(@RequestBody Customer customer)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(customer);
+	}
+	
 
 }
